df8368099f2bc09802acb799eaf73604cfe9e258
giovanneguido7 <giovanneguido7@gmail.com>
2024-03-08 15:23:57 -0300

    2

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>7</Text>
+      <View style={styles.button}> 
+        <Button title='MAIS'/>
+        <Button title='MENOS'/>
+      </View>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -17,4 +21,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#000',
+    fontSize: 60,
+  },
+
+  button: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    width: 150,
+
+  },
+
 });
