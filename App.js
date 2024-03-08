c8b641ee3ffd404e76126bc3ca680ced0ab896ca
giovanneguido7 <giovanneguido7@gmail.com>
2024-03-08 16:12:32 -0300

    Adicionei botao de funcoes

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [mais, setMais] = useState(0);
+
+  const incrementCounter = () => {
+    setMais(mais + 1);
+  };
+  const decrementCounter = () => {
+    setMais(mais - 1);
+  };
+
+  const zerarCounter = () => {
+    setMais(0);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>7</Text>
-      <View style={styles.button}> 
-        <Button title='MAIS'/>
-        <Button title='MENOS'/>
+      <Text style={styles.text}>{mais}</Text>
+      <View style={styles.button}>
+        <Button onPress={incrementCounter} title="+" />
+        <Button onPress={decrementCounter} title="-" />
       </View>
-      <StatusBar style='auto' />
+      <View style={styles.zero}>
+        <Button onPress={zerarCounter} title="Zerar" />
+      </View>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -17,21 +34,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: '#000',
+    color: "#000",
     fontSize: 60,
   },
-
   button: {
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    width: 150,
-
+    marginTop: 50,
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row-reverse",
+    width: 250,
   },
-
+  zero: {
+    fontSize: 40,
+  },
 });
